bbb70c5d10d9799c04155f294c57ef040465fdf7
ravishankar <ravishankar.rupakula@walmart.com>
2020-04-30 13:51:20 +0530

    changes

--- example.py
+++ example.py
@@ -4,4 +4,5 @@ sys.path.append(".") # add local directory as a module path
 import my_module
 
 # 'execution' is the Concord process' context, 'x' is a Concord process' variable
-my_module.test(execution, jsonResponse)
+x = execution.getVariable("jsonResponse.content")
+my_module.test(execution, x)
