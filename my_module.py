789dcb6b6933c9bfbd99aefd3b902400a5708b1e
ravishankar <ravishankar.rupakula@walmart.com>
2020-04-30 15:48:42 +0530

    changes

--- my_module.py
+++ my_module.py
@@ -1,3 +1,7 @@
+imoprt json
+
 def test(ctx, x):
     print 'Hello from another module!'
-    ctx.setVariable('y', x)
+    ctx.setVariable('y')
+    json_data = json.loads(x)
+    y = json_data['uid']
