0adea3dd6697d0dfc83cf8a9cc041755f9bee636
ravishankar <ravishankar.rupakula@walmart.com>
2020-04-30 16:21:39 +0530

    changes

--- my_module.py
+++ my_module.py
@@ -3,6 +3,4 @@ import json
 def test(ctx, x):
     print 'Hello from another module!'
     ctx.setVariable('y',1)
-    json_data = json.loads(x)
-    print json_data
-    #y = str(json_data["uid"])
+    y = json.dumps(x)
