788a77dd49f870cd9a806758589c2faccddbd715
ravishankar <ravishankar.rupakula@walmart.com>
2020-05-12 15:39:49 +0530

    changes

--- target/my_logic2.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append(".") # add local directory as a module path
-
-import my_module2
-
-# 'execution' is the Concord process' context, 'x' is a Concord process' variable
-my_module2.test(execution, itemcounts)
--- target/my_module2.py
+++
@@ -1,8 +0,0 @@
-import json
-
-def test(ctx, itemcounts):
-    print 'i am inside the second python module...'
-    finalcount = 0
-    for i in itemcounts:
-        finalcount = finalcount + i
-    ctx.setVariable('finalcount', finalcount)
